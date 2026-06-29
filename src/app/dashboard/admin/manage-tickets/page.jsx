2b7fdb9c85e9231ee2bfd037daf0e2066f44c2d1
arifkhanever <mdshafiqul0404@gmail.com>
2026-06-29 16:46:33 +0600

    project finalized for deployment

--- src/app/dashboard/admin/manage-tickets/page.jsx
+++ src/app/dashboard/admin/manage-tickets/page.jsx
@@ -1,12 +1,15 @@
 import ManageTicketsClient from '@/Components/dashboard/admin/ManageTicketsCllient';
 import { getTickets } from '@/lib/api/tickets';
 import React from 'react';
+export const dynamic = "force-dynamic";
 
 const ManageTicketsPage = async() => {
-  const tickets = await getTickets({role:"admin"})
+
+  const response = await getTickets({role:"admin"})
+  const tickets = response?.tickets || [];
   return (
     <div>
-      <ManageTicketsClient tickets={tickets.tickets}/>
+      <ManageTicketsClient tickets={tickets}/>
     </div>
   );
 };
